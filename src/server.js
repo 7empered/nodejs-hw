635b9a52cdc71f87e2b1b9d30340ab41ce91d09f
Bogdan Stetsenko <stetsenkobogdan0@gmail.com>
2026-09-01 04:01:47 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ import express from 'express';
 import cors from 'cors';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
-import logger from './middleware/logger.js';
-import notFoundHandler from './middleware/notFoundHandler.js';
-import errorHandler from './middleware/errorHandler.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 import notesRouter from './routes/notesRoutes.js';
 
 const PORT = process.env.PORT || 3000;
