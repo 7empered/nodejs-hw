52775083ce934666068626ff3ae9a35dcf74dbdf
Bogdan Stetsenko <stetsenkobogdan0@gmail.com>
2026-09-11 12:05:17 +0300

    validation hw fix

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,30 +1,31 @@
 import createHttpError from 'http-errors';
-import Note from '../models/note.js';
+import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res, next) => {
   try {
     const { page = 1, perPage = 10, tag, search } = req.query;
 
-    let notesQuery = Note.find();
+    const filter = {};
 
     if (tag) {
-      notesQuery = notesQuery.where('tag').equals(tag);
+      filter.tag = tag;
     }
 
     if (search) {
-      notesQuery = notesQuery.where({
-        $or: [
-          { title: { $regex: search, $options: 'i' } },
-          { content: { $regex: search, $options: 'i' } },
-        ],
-      });
+      filter.$or = [
+        { title: { $regex: search, $options: 'i' } },
+        { content: { $regex: search, $options: 'i' } },
+      ];
     }
 
-    const totalNotes = await notesQuery.clone().countDocuments();
-    const totalPages = Math.ceil(totalNotes / perPage);
     const skip = (page - 1) * perPage;
 
-    const notes = await notesQuery.skip(skip).limit(perPage);
+    const [notes, totalNotes] = await Promise.all([
+      Note.find(filter).skip(skip).limit(perPage),
+      Note.countDocuments(filter),
+    ]);
+
+    const totalPages = Math.ceil(totalNotes / perPage);
 
     res.status(200).json({
       page: Number(page),
