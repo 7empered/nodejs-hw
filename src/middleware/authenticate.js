779147ae2441d23be289732011cd8fa97e41957f
Bogdan Stetsenko <stetsenkobogdan0@gmail.com>
2026-09-17 01:17:22 +0300

    middleware fix

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -10,7 +10,7 @@ export const authenticate = async (req, res, next) => {
       throw createHttpError(401, 'Missing access token');
     }
 
-    const session = await Session.findOne({ accessToken });
+    const session = await Session.findOne({ _id: sessionId, accessToken });
     if (!session) {
       throw createHttpError(401, 'Session not found');
     }
